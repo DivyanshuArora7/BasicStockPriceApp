af713d1feae3975bb72dbd1a43647ba3e163f554
Divyanshu Arora <130824506+DivyanshuArora7@users.noreply.github.com>
2023-04-15 14:03:10 +0530

    Update Stock Price.py

--- Stock Price.py
+++ Stock Price.py
@@ -2,13 +2,16 @@ import yfinance as yf
 import streamlit as st
 
 st.write("""
-# Simple Stock Price App
-Shown are the stock **closing price** and ***volume*** of Google!
+Basic Stock Price App
+======
+> ### Shown are the stock **closing price** and **volume** of Amazon.com
+> [Amazon.com](https://www.amazon.com)
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#  Source: https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
-tickerSymbol = 'GOOGL'
+#You can change the tickerSymbol(We used 'AMZN' here)(Search on Google for tickerSymbols of different companies like Apple)
+tickerSymbol = 'AMZN'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
@@ -23,3 +26,6 @@ st.write("""
 ## Volume Price
 """)
 st.line_chart(tickerDf.Volume)
+st.write("""
+> #### PROJECT BY: **DIVYANSHU ARORA**
+""")
